7123bb9d155a4b1874bbf0a6ee68d3e6b30d05ed
Miraz <mdmirazhowladermiraz491@gmail.com>
2026-08-22 15:27:35 +0600

    Add views for dim_customers and fact_transactions
    Created views for customers and transactions from the bronze data.

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -14,6 +14,50 @@ Usage:
 ===============================================================================
 */
 
--- ============================================================================
--- Create Dimension: gold.dim_customers
--- ============================================================================
+--===========================================
+--Create Dimension : gold.dim_customers
+--===========================================
+if object_id ('gold.dim_customers','V') is not null
+drop table gold.dim_customers;
+go
+    CREATE VIEW gold.dim_customers AS
+    SELECT 
+    ROW_NUMBER() OVER(ORDER BY customer_id) AS Customer_KEY,
+    customer_id,
+    account_number,
+    full_name AS customer_name,
+    account_type,
+    branch,
+    account_status,
+    city,
+    country,
+    account_open_date,
+    email,
+    phone
+FROM silver.bronze_customers_accounts;
+go
+
+--===========================================
+--Create Fact Table: gold.fact_transactions
+--===========================================
+if object_id ('gold.fact_transactions','V') is not null
+drop table gold.fact_transactions;
+go
+
+    CREATE VIEW gold.fact_transactions AS
+    SELECT 
+    ROW_NUMBER() OVER(ORDER BY transaction_id) AS transaction_KEY,
+    transaction_id,
+    account_number,
+    transaction_date,
+    amount_signed,
+    currency,
+    transaction_type,
+    channel,
+    status,
+    is_amount_missing,
+    is_amount_outlier,
+    is_orphan_account,
+    is_predates_account_open
+FROM silver.bronze_transactions
+go
